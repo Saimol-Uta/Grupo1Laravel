1369a04e282c5c981123858748f9f5f5673824cc
Saimol-Uta <sjimenez6271@uta.edu.ec>
2025-10-07 23:01:12 -0500

    init

--- Grupo1/routes/web.php
+++ Grupo1/routes/web.php
@@ -4,6 +4,10 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AlumnosController;
 use App\Http\Controllers\RegistroDeCursoController;
 
+// el resource crea rutas para las operaciones CRUD
+//ejemplo: GET /Alumnos -> index, GET /Alumnos/create -> create, POST /Alumnos -> store, etc.
+// esto se crea automaticamente al usar Route::resource
+
 Route::resource('Alumnos', AlumnosController::class);
 Route::resource('RegistroDeCurso', RegistroDeCursoController::class);
 
